5840bd3ef4e3225a9ffd7ea14977cd78542f0525
Tanmayi Manku <tanmayitanum@gmail.com>
2024-07-19 19:20:50 +0530

    window inbuilt method testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 const App = () => {
   const [count, setCount] = useState(0);
 
+  // getting the url path for a webpage using javascript.
+  const newURL = `${window.location.protocol}//${window.location.host}/${window.location.pathname}/${window.location.search}`;
+
   const IncrementCount = () => {
     setCount(count + 1);
   }
@@ -20,6 +23,7 @@ const App = () => {
         <button type="button" onClick={DecrementCount}>Decremment</button>
         <button type="button" onClick={IncrementCount}>Incremment</button>
       </div>
+      <h3>the URL got from the window is: {newURL}</h3>
     </div>
   );
 };
